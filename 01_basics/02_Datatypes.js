aaa9f81ef29ebacf5b2c42a5caac08abca25055f
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-01-19 21:39:25 +0000

    datatypes practice file

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all javascript as newer version
+
+//alert (3+3) // we are using node.js not browser
+/* console.log(
+    3+
+     3);*/   // code readability should be high unlike this unreadable code
+
+let name = "Asad";
+let age = 20;
+let isLoggedIn= true;
+let state = null;
+let nature;
+
+// Number => integers in range of 2 to power 52 
+// bigInt => for big numbers
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+
+// object => 
+
+console.log(typeof state)
+console.log(typeof nature)
