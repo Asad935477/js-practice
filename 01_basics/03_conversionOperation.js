4d2e580395f1bf926da7def9c68812cacef8d636
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-01-19 22:27:02 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,73 @@
+/*
+let score = "200points"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+console.log(valueInNumber) // the output NaN stands for "Not a Number"
+
+let serverRank = null
+console.log(typeof(serverRank))
+console.log(typeof serverRank)
+
+let rankInNumber = Number(serverRank)
+
+console.log(typeof(rankInNumber))
+console.log(rankInNumber)
+
+let operatorSkill = undefined
+console.log(typeof operatorSkill)
+console.log(typeof(operatorSkill))
+
+let skillInNumber = Number(operatorSkill)
+
+console.log(typeof(skillInNumber))
+console.log(skillInNumber)
+
+let pro = true
+console.log(typeof pro)
+console.log(typeof(pro))
+
+let proInNumber = Number(pro)
+
+console.log(typeof(proInNumber))
+console.log(proInNumber)
+*/
+
+
+//---Number Conversions---
+
+//"100" => 100
+//"100points" => NaN (type still Number)
+// true = 1 ; false => 0
+
+
+
+//---------------------------------
+
+/*
+let loggedIn = 1
+let boolLog = Boolean(loggedIn)
+
+console.log(typeof boolLog)
+
+let loggedOut = 0
+let boolOut = Boolean(loggedOut)
+
+console.log(typeof boolOut)
+console.table ([boolOut, boolLog])
+*/
+
+
+//---Boolean Conversions---
+
+// 0 => false
+// 1 => true
+// '' => false 
+// 'asad' => true
+
+
+
+//-----------------------------------
