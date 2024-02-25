7d0bc8840f8c3591301d9d0255b2cb896472ecb6
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-02-25 19:16:44 +0000

    todays practice

--- MISC CODES/practice 1.js
+++ MISC CODES/practice 1.js
@@ -146,20 +146,17 @@ const data = [
 
 // Destructuring
 
-
 const book = getBook(3);
 book;
 
 // const title = book.title;
 // const author = book.author;
 
-const { title, author, pages, publicationDate, genres, hasMovieAdaptation } = 
-  book;
-
+const {title, author, genres, id, pages, publicationDate, hasMovieAdaptation} = book;
 console.log(author, title, genres);
 
 // const primaryGenre = genres[0];
-// const secondaryGenre = genres[1];
+// const secondaryGenre = genres[1]
 
-const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
-console.log(primaryGenre, secondaryGenre, otherGenres);
+const [primaryGenre, ...otherGenres] = genres;
+
